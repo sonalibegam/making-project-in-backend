a199f63e317a60374c76f6564c69002334809004
23IUT0010107 <begamsonali586@gmail.com>
2025-09-27 19:55:14 +0530

    Done

---
+++ src/app.js
@@ -0,0 +1,62 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb" }))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export { app }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// middleware hoise amra jokhon ekta url req kori tokhon directly je result ta aiye edar majher jinish ta.
