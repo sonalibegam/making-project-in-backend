0e769235c36ea6d5982cd5ab8856deca0a44e8e3
23IUT0010107 <begamsonali586@gmail.com>
2025-09-27 17:22:16 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+// always try to wrap in try-catch for connecting to database otherwise will have gto face issues --------also use async
+/*
+import express from 'express'
+const app = express()
+
+
+
+(async() => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_UR}/${DB_NAME}`)
+       app.on("error", (error) => {
+        console.log("ERRR:", error);
+        throw error
+        
+       })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.Port}`);
+        
+    })
+
+    } catch (error) {
+        console.error("Error", error)
+        throw err
+    }
+})()
+
+*/
