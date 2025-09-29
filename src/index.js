e87036f4cfebe1b8e4b24e49b62c1d64458be869
23IUT0010107 <begamsonali586@gmail.com>
2025-09-29 20:47:28 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-//require('dotenv').config({path:'./env'})
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from './db/index.js'
-
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-        
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
-    
+    console.log("MONGO db connection failed !!! ", err);
 })
 
 
@@ -27,29 +27,23 @@ connectDB()
 
 
 
-// always try to wrap in try-catch for connecting to database otherwise will have gto face issues --------also use async
 /*
-import express from 'express'
+import express from "express"
 const app = express()
-
-
-
-(async() => {
+( async () => {
     try {
-       await mongoose.connect(`${process.env.MONGODB_UR}/${DB_NAME}`)
-       app.on("error", (error) => {
-        console.log("ERRR:", error);
-        throw error
-        
-       })
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
-    app.listen(process.env.PORT, () => {
-        console.log(`App is listening on port ${process.env.Port}`);
-        
-    })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
 
     } catch (error) {
-        console.error("Error", error)
+        console.error("ERROR: ", error)
         throw err
     }
 })()
