6506e01e873f64fe9da54f7d6a0f3f54df934464
23IUT0010107 <begamsonali586@gmail.com>
2025-09-29 19:55:09 +0530

    add

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import { response } from "express";
+import fs from "fs"
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload the file on cloudinary
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        }) 
+        // file has been uploaded successfully
+        console.log("file is uploaded in cloudinary", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload opertaion got failed
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
+
